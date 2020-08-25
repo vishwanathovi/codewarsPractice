3dcf70f8a85460296508659767628714b5375b84
haripallikere <haripallikere93@gmail.com>
2020-08-25 17:15:50 +0530

    first solution

---
+++ Array-Deep-Count/index.js
@@ -0,0 +1,17 @@
+// count all the elements in an array including the nested one;
+
+//Parameters: An array of elemets,(arrOfEle);
+//Return: number of all elements in array,(totalElemet);
+
+//test cases:
+ print(deepCount([1,2,3]) === 3);
+ print(deepCount([[1,2,3],[13,34,12],55,'fg']) === 10);
+
+function deepCount(arrOfEle) {
+    // count all elements in an nexted array
+    // (if array contains another array of elements take array as single element and 
+    // again take length of it and sum it up)
+    // use of loop and condition
+    // use of reduce method and conditions
+
+}
